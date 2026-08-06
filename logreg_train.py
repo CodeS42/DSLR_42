cf6294fef901546f4f3add68bc7299431bcbd7b2
csy <csy@f4r1s15.paris.42.school>
2026-08-06 11:09:44 +0200

    norm

--- logreg_train.py
+++ logreg_train.py
@@ -4,6 +4,7 @@ import numpy as np
 import json as js
 import matplotlib.pyplot as plt
 
+
 def ft_mean(values):
     res = 0.0
     count = 0
@@ -16,6 +17,7 @@ def ft_mean(values):
         return 0
 
     return res / count
+
 
 def ft_std(values, mean):
     res = 0.0
@@ -32,12 +34,15 @@ def ft_std(values, mean):
 
     return np.sqrt(variance)
 
+
 # z -> multi matri
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
+
 def predict(X, theta):
     return sigmoid(X @ theta)
+
 
 # m -> num samples
 # y -> target class
@@ -45,19 +50,20 @@ def cost_function(X, y, theta):
     epsilon = 1e-15
     m = len(X)
     h = predict(X, theta)
-    cost = (-1/m) * np.sum(y * np.log(h + epsilon) + (1 - y) * np.log(1 - h + epsilon))
+    cost = (-1/m) * np.sum(y * np.log(
+        h + epsilon) + (1 - y) * np.log(1 - h + epsilon))
 
     return cost
 
+
 def gradient_descent(learning_rate, theta, X, y):
     h = predict(X, theta)
-    m = len(X)
     err = (h - y).reshape(-1)
 
     gradient = np.zeros((X.shape[1], 1))
     for i in range(X.shape[1]):
         gradient[i] = ft_mean(X[:, i] * err)
-    
+
     return theta - learning_rate * gradient
 
 
@@ -83,7 +89,7 @@ class logisticRegression:
                 standard[name] = values - mean_value
             else:
                 standard[name] = (values - mean_value) / std_value
-        
+
         return pd.DataFrame(standard)
 
     def fit(self, features, target, learning_rate=0.1, iterations=10000):
@@ -107,19 +113,21 @@ class logisticRegression:
                 # cost = cost_function(X, y, self.theta).item()
                 # print(f"iteration {i:5d} | cost = {cost:.6f}")
                 # cost_history.append(cost)
-        
+
         return cost_history
 
 
 def main():
     if len(sys.argv) != 2:
         raise SystemExit("Wrong number of arguments.")
-    
+
     path = sys.argv[1]
     df = pd.read_csv(path).dropna()
 
-    all_features = df.select_dtypes(include=np.number).drop(columns=["Index"]).columns
-    drop_features = ["Arithmancy", "Potions", "Transfiguration", "Care of Magical Creatures"]
+    all_features = df.select_dtypes(
+        include=np.number).drop(columns=["Index"]).columns
+    drop_features = ["Arithmancy", "Potions",
+                     "Transfiguration", "Care of Magical Creatures"]
     selected_features = all_features.drop(drop_features)
     X_df = df[selected_features]
 
@@ -138,7 +146,7 @@ def main():
 
     for i, house in enumerate(houses):
         model = logisticRegression()
-        binary_target = (target == house).astype(int) # one vs all
+        binary_target = (target == house).astype(int)  # one vs all
 
         history_cost = model.fit(
             X_df,
@@ -150,7 +158,7 @@ def main():
         if not model_stat:
             model_stat['feature_mean'] = model.feature_mean
             model_stat['feature_std'] = model.feature_std
-        
+
         axes[i].plot(
             range(0, len(history_cost) * 300, 300),
             history_cost
@@ -172,7 +180,7 @@ def main():
     #             f"{feature:30} {weight:10.6f}"
     #             f"  abs ={abs(weight):12.6f}"
     #         )
-    
+
     # save
     model_data = {
         'thetas': final_theta,
